c60d3e3a587791b0e80c7d796976a601f36b2952
Vortriz <vorarishi22+github@gmail.com>
2025-10-11 13:23:18 +0530

    init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells = {
+        default = pkgs.mkShell {
+          packages = [pkgs.typst];
+        };
+      };
+    });
+}
